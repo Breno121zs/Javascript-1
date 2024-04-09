073dc3b45a5a26887a38e96c91eae782f4a6ea8c
Breno1221 <123486496+Breno121zs@users.noreply.github.com>
2024-04-09 10:27:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log (100);
+
+console.log ('hello world')
+
+const x = 100
+
+console.log (x);
+
+console.error('Alert');
+
+console.warn('Warning');
+
+console.table({ name: 'brad', email: 'brad@gmail.com'});
+
+console.group ('simple');
+console.log (x);
+console.error('Alert');
+console.warn('Warning');
+console.groupEnd();
